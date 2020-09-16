c468a732ffd122baf3022e568f498b76159629d9
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-16 22:59:18 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  my_hash = {"name" => "Todd", "age" => 100}
 
   
 end
@@ -21,6 +22,8 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  
+  shipping_manifest["oil paintings"]
 
 end
 
@@ -31,13 +34,15 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
-
+  
+  shipping_manifest["muskets"]= 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+  shipping_manifest["gun powder"]= 4
 
   # return the shipping_manifest hash below
+  
+  shipping_manifest
 
 end
