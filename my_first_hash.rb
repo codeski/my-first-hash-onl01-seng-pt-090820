9a408aa1ffeef285243ef65df39c07e1d23e5341
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-16 23:39:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,7 +15,7 @@ def shipping_manifest
   #the_manifest["whale bone corsets"] = 5
   #the_manifest["porcelain vases"] = 2 
   #the_manifest["oil paintings"] = 3
-  shipping_manifest
+  the_manifest
 end
 
 def retrieval
